293656576cd34a76aa3ca7e7295b03c3549e7b67
Michael Edlund <medlund@mac.com>
2013-07-29 16:50:58 -0700

    Listing accept_nested_attributes_for enumerals.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -11,13 +11,13 @@ class Listing < ActiveRecord::Base
   belongs_to :community
   belongs_to :foreclosure_status
 
-  has_many :expenses
-  has_many :open_houses
-  has_many :rooms
-  has_many :taxes
-  has_many :listing_photos
-  has_many :listing_videos
-  has_many :virtual_tours
+  has_many :expenses, :dependent => :destroy
+  has_many :open_houses, :dependent => :destroy
+  has_many :rooms, :dependent => :destroy
+  has_many :taxes, :dependent => :destroy
+  has_many :listing_photos, :dependent => :destroy
+  has_many :listing_videos, :dependent => :destroy
+  has_many :virtual_tours, :dependent => :destroy
   
   has_and_belongs_to_many :addresses
   has_and_belongs_to_many :listing_participants
@@ -39,6 +39,17 @@ class Listing < ActiveRecord::Base
   has_and_belongs_to_many :roof_materials, association_foreign_key: "enumeral_id"
   has_and_belongs_to_many :parking, association_foreign_key: "enumeral_id"
   has_and_belongs_to_many :views, association_foreign_key: "enumeral_id"
+  
+  accepts_nested_attributes_for :appliances
+  accepts_nested_attributes_for :cooling_systems
+  accepts_nested_attributes_for :exterior_types
+  accepts_nested_attributes_for :flooring_materials
+  accepts_nested_attributes_for :heating_fuels
+  accepts_nested_attributes_for :heating_systems
+  accepts_nested_attributes_for :home_features
+  accepts_nested_attributes_for :roof_materials
+  accepts_nested_attributes_for :parking
+  accepts_nested_attributes_for :views
   
   validates_presence_of :list_price
   validates_presence_of :property_type
