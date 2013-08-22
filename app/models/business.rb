6cf8b85aa6935c4488583b1a84f3b2d3df9d8dde
Michael Edlund <medlund@mac.com>
2013-08-22 10:22:10 -0700

    :businesses habtm :listings

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,6 +1,7 @@
 class Business < ActiveRecord::Base
 
   has_and_belongs_to_many :addresses, foreign_key: "business_id"
+  has_and_belongs_to_many :listings, foreign_key: "business_id"
 
   validates_presence_of :name
   validates_presence_of :type
