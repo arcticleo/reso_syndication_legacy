0ca9b15b4d287598ae43491995cfe6bec2264ee3
Michael Edlund <medlund@mac.com>
2014-05-02 13:12:45 -0700

    Added indexes to some join tables. Rails 4+. Bumped v. to 0.9.7.

--- lib/generators/rets_data/templates/create_join_tables.rb
+++ lib/generators/rets_data/templates/create_join_tables.rb
@@ -5,24 +5,30 @@ class CreateJoinTables < ActiveRecord::Migration
 
     create_join_table :addresses, :listings
     add_index(:addresses_listings, [:address_id, :listing_id], :unique => true)
+    add_index(:addresses_listings, [:listing_id])
 
     create_join_table :addresses, :listing_offices
     add_index(:addresses_listing_offices, [:address_id, :listing_office_id], :unique => true, :name => "index_address_offices_address_id_office_id")
 
     create_join_table :businesses, :listings
     add_index(:businesses_listings, [:business_id, :listing_id], :unique => true)
+    add_index(:listings, [:listing_id])
 
     create_join_table :enumerals, :listings
     add_index(:enumerals_listings, [:enumeral_id, :listing_id], :unique => true)
+    add_index(:enumerals_listings, [:listing_id])
 
     create_join_table :listing_offices, :listings
     add_index(:listing_offices_listings, [:listing_office_id, :listing_id], :unique => true, :name => "index_listings_offices_listing_id_office_id")
+    add_index(:listing_offices_listings, [:listing_id])
 
     create_join_table :listings, :participants
     add_index(:listings_participants, [:participant_id, :listing_id], :unique => true)
+    add_index(:listings_participants, [:listing_id])
 
     create_join_table :listings, :places
     add_index(:listings_places, [:listing_id, :place_id], :unique => true)
+    add_index(:listings_places, [:listing_id])
 
     create_join_table :places, :schools
     add_index(:places_schools, [:place_id, :school_id], :unique => true)
