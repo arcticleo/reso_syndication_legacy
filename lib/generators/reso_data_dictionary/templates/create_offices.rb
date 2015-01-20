ee2fb3a67ab67e903bba9346c24b9b74bac40919
Michael Edlund <medlund@mac.com>
2015-01-20 10:22:48 -0800

    Added more fields to Office.

--- lib/generators/reso_data_dictionary/templates/create_offices.rb
+++ lib/generators/reso_data_dictionary/templates/create_offices.rb
@@ -6,13 +6,19 @@ class CreateOffices < ActiveRecord::Migration
       t.string :level
       t.string :office_code_identifier
       t.string :name, :null => false
+      t.string :franchise_affiliation
       t.string :corporate_name
       t.string :broker_identifier
       t.string :main_office_identifier
       t.string :phone_number
-      t.string :fax_number
+      t.string :office_fax
       t.string :office_email
       t.string :website
+      t.boolean :idx_office_participation_yn
+      t.string :main_office_key
+      t.string :main_office_mlsid
+      t.string :office_broker_key
+      t.string :office_broker_mlsid
 
       t.timestamps
     end
