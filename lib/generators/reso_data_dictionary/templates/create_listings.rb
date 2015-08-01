c0239a76e49e43df87418842e1293892223909ab
Michael Edlund <medlund@mac.com>
2015-08-01 12:51:01 -0700

    Sorted Listing fields in migration.

--- lib/generators/reso_data_dictionary/templates/create_listings.rb
+++ lib/generators/reso_data_dictionary/templates/create_listings.rb
@@ -1,64 +1,64 @@
 class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings, options: 'DEFAULT CHARSET=utf8' do |t|
-      t.references :architecture_style, index: true
-      t.string     :architecture_style_description
-      t.integer    :bathrooms
-      t.integer    :bedrooms
-      t.string     :currency_code, default: "USD"
-      t.boolean    :disclose_address
-      t.string     :lead_routing_email
-      t.integer    :list_price, limit: 8
-      t.integer    :list_price_low, limit: 8
-      t.references :listing_category, index: true, null: false
-      t.text       :listing_description
-      t.string     :listing_key, null: false
-      t.references :listing_provider, index: true
-      t.references :listing_status, index: true, null: false
-      t.string     :listing_url
-      t.references :mls, index: true
-      t.string     :mls_number
-      t.boolean    :permit_address_on_internet
-      t.references :property_sub_type, index: true, null: false
-      t.string     :property_sub_type_description
-      t.references :property_type, index: true, null: false
-      t.string     :property_type_description
-      t.boolean    :short_sale
-      t.boolean    :vow_address_display
-      t.boolean    :vow_automated_valuation_display
-      t.boolean    :vow_consumer_comment
-      t.integer :living_area
-      t.string :living_area_unit, default: "SQFT"
-      t.float :lot_size
-      t.string :lot_size_unit
-      t.date :listing_date
-      t.string :tracking_item
-      t.text :listing_title
-      t.integer :full_bathrooms
-      t.integer :three_quarter_bathrooms
-      t.integer :half_bathrooms
-      t.integer :one_quarter_bathrooms
-      t.integer :partial_bathrooms
-      t.decimal :latitude, precision: 10, scale: 6
-      t.decimal :longitude, precision: 10, scale: 6
-      t.references :county, index: true
-      t.references :community, index: true
-      t.text :directions
-      t.string :elevation
-      t.string :geocode_options
-      t.string :parcel_info
-      t.references :zoning_type, index: true
-      t.integer :year_built
-      t.integer :year_updated
-      t.integer :building_unit_count
-      t.integer :num_floors
-      t.integer :condo_floor_num
-      t.integer :num_parking_spaces
-      t.integer :room_count
-      t.text :legal_description
-      t.references :foreclosure_status, index: true
-      t.string :modification_timestamp
-      t.string :disclaimer
+      t.references  :architecture_style, index: true
+      t.string      :architecture_style_description
+      t.integer     :bathrooms
+      t.integer     :bedrooms
+      t.integer     :building_unit_count
+      t.references  :community, index: true
+      t.integer     :condo_floor_num
+      t.references  :county, index: true
+      t.string      :currency_code, default: "USD"
+      t.text        :directions
+      t.string      :disclaimer
+      t.boolean     :disclose_address
+      t.string      :elevation
+      t.references  :foreclosure_status, index: true
+      t.integer     :full_bathrooms
+      t.string      :geocode_options
+      t.integer     :half_bathrooms
+      t.decimal     :latitude, precision: 10, scale: 6
+      t.string      :lead_routing_email
+      t.text        :legal_description
+      t.integer     :list_price, limit: 8
+      t.integer     :list_price_low, limit: 8
+      t.references  :listing_category, index: true, null: false
+      t.date        :listing_date
+      t.text        :listing_description
+      t.string      :listing_key, null: false
+      t.references  :listing_provider, index: true
+      t.references  :listing_status, index: true, null: false
+      t.text        :listing_title
+      t.string      :listing_url
+      t.integer     :living_area
+      t.string      :living_area_unit, default: "SQFT"
+      t.decimal     :longitude, precision: 10, scale: 6
+      t.float       :lot_size
+      t.string      :lot_size_unit
+      t.references  :mls, index: true
+      t.string      :mls_number
+      t.string      :modification_timestamp
+      t.integer     :num_floors
+      t.integer     :num_parking_spaces
+      t.integer     :one_quarter_bathrooms
+      t.string      :parcel_info
+      t.integer     :partial_bathrooms
+      t.boolean     :permit_address_on_internet
+      t.references  :property_sub_type, index: true, null: false
+      t.string      :property_sub_type_description
+      t.references  :property_type, index: true, null: false
+      t.string      :property_type_description
+      t.integer     :room_count
+      t.boolean     :short_sale
+      t.integer     :three_quarter_bathrooms
+      t.string      :tracking_item
+      t.boolean     :vow_address_display
+      t.boolean     :vow_automated_valuation_display
+      t.boolean     :vow_consumer_comment
+      t.integer     :year_built
+      t.integer     :year_updated
+      t.references  :zoning_type, index: true
 
       t.timestamps
     end
