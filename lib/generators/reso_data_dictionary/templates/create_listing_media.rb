af382b029b8957a096f5caf50df2784108e92c9d
Michael Edlund <medlund@mac.com>
2015-08-02 12:36:42 -0700

    Added support for MediaOrderNumber to ListingMedia.

--- lib/generators/reso_data_dictionary/templates/create_listing_media.rb
+++ lib/generators/reso_data_dictionary/templates/create_listing_media.rb
@@ -1,6 +1,7 @@
 class CreateListingMedia < ActiveRecord::Migration
   def change
     create_table :listing_media, options: 'DEFAULT CHARSET=utf8' do |t|
+      t.integer :media_order_number
       t.string :media_url, null: false, limit: 1024 
       t.string :media_caption
       t.text :media_description
@@ -10,6 +11,7 @@ class CreateListingMedia < ActiveRecord::Migration
 
       t.timestamps
     end
+    add_index :listing_media, :media_order_number
     add_index :listing_media, :type
   end
 end
