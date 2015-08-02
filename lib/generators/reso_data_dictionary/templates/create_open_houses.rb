366ef464292a3f0db60d40fdb83d7387d0eeb137
Michael Edlund <medlund@mac.com>
2015-08-02 11:58:37 -0700

    Make OpenHouse start, end times strings while figuring out time zones.

--- lib/generators/reso_data_dictionary/templates/create_open_houses.rb
+++ lib/generators/reso_data_dictionary/templates/create_open_houses.rb
@@ -2,8 +2,10 @@ class CreateOpenHouses < ActiveRecord::Migration
   def change
     create_table :open_houses, options: 'DEFAULT CHARSET=utf8' do |t|
       t.date :showing_date
-      t.time :start_time
-      t.time :end_time
+
+      # TODO: Does RESO provide a way to specify time zone?
+      t.string :start_time
+      t.string :end_time
       t.text :description
       t.references :listing, index: true, null: false
 
