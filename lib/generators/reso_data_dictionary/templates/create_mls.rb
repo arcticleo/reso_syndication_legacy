da8412da3bc3cdd604f8d649a92d9389807e744f
Michael Edlund <medlund@mac.com>
2015-08-02 11:21:33 -0700

    Removed MLS name and id requirement.

--- lib/generators/reso_data_dictionary/templates/create_mls.rb
+++ lib/generators/reso_data_dictionary/templates/create_mls.rb
@@ -1,8 +1,8 @@
 class CreateMls < ActiveRecord::Migration
   def change
     create_table :mls, options: 'DEFAULT CHARSET=utf8' do |t|
-      t.string :mls_id, null: false
-      t.string :mls_name, null: false
+      t.string :mls_id
+      t.string :mls_name
 
       t.timestamps
     end
