a0eae1c9da793da97064cc3296ac9becb19256f4
Michael Edlund <medlund@mac.com>
2015-08-09 08:27:32 -0700

    Set RESO to default import_format for Import.

--- app/models/import.rb
+++ app/models/import.rb
@@ -5,4 +5,10 @@ class Import < ActiveRecord::Base
   has_many :listings
   has_many :queued_listings
 
+  before_save :set_import_format
+
+  def set_import_format
+    self.import_format = ImportFormat.find_by(name: 'reso') unless self.import_format.present?
+  end
+
 end
