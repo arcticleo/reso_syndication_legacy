32d37550764f74e8d55333e60bfff2fe0d12eab8
Michael Edlund <medlund@mac.com>
2015-08-09 18:56:58 -0700

    Break down US street addresses in their address components.

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,22 @@ class Address < ActiveRecord::Base
   belongs_to :address_type
   belongs_to :addressable, polymorphic: true
 
+  before_save :validate_us_address
+  
+  def address_concatenated
+    [self.full_street_address, self.city, self.state_or_province, self.postal_code].join(', ')
+  end
+  
+  def validate_us_address
+    if (address = StreetAddress::US.parse(self.address_concatenated))
+      self.street_number = address.number
+      self.street_dir_prefix = address.prefix
+      self.street_name = address.street
+      self.street_suffix = address.street_type
+      self.street_dir_suffix = address.suffix
+      self.unit_number = address.unit unless address.unit.blank?
+      self.country = 'US'
+    end
+  end
+
 end
