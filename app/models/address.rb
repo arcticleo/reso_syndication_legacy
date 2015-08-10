563070e6e2f954e9aaf8d37bbeb83a755314842e
Michael Edlund <medlund@mac.com>
2015-08-09 19:06:18 -0700

    Set state_or_province to state from sanitized address.

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,13 +3,13 @@ class Address < ActiveRecord::Base
   belongs_to :address_type
   belongs_to :addressable, polymorphic: true
 
-  before_save :validate_us_address
+  before_save :validate_and_sanitize_us_address
   
   def address_concatenated
     [self.full_street_address, self.city, self.state_or_province, self.postal_code].join(', ')
   end
   
-  def validate_us_address
+  def validate_and_sanitize_us_address
     if (address = StreetAddress::US.parse(self.address_concatenated))
       self.street_number = address.number
       self.street_dir_prefix = address.prefix
@@ -17,6 +17,7 @@ class Address < ActiveRecord::Base
       self.street_suffix = address.street_type
       self.street_dir_suffix = address.suffix
       self.unit_number = address.unit unless address.unit.blank?
+      self.state_or_province = address.state
       self.country = 'US'
     end
   end
