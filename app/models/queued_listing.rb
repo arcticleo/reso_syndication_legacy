874b633970397fb6094ea1f64cecf97f37ea013f
Michael Edlund <medlund@mac.com>
2015-08-14 17:08:53 -0700

    Tweaked eager_loading when creating listing.

--- app/models/queued_listing.rb
+++ app/models/queued_listing.rb
@@ -8,9 +8,14 @@ class QueuedListing < ActiveRecord::Base
   end
 
   def create_or_update_listing
-    listing = self.import.listings.eager_load(:address).eager_load(:photos).find_or_initialize_by(
-      listing_key: mapper.unique_identifier(self)
-    )
+    listing = self.import.listings.
+      eager_load(:address).
+      eager_load(:appliances).
+      eager_load(:participants).
+      eager_load(:photos).
+      find_or_initialize_by(
+        listing_key: mapper.unique_identifier(self)
+      )
     if (listing.modification_timestamp != mapper.modification_timestamp(self, listing))
       Mapper::RESO_LISTING_ATTRIBUTES.each do |attribute|
         listing.send("#{attribute}=", mapper.send(attribute, self, listing))
