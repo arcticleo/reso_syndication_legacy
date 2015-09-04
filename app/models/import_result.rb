6de8fc5fc7a210f6d02ea9d32d5c6a3d09a791cb
Michael Edlund <medlund@mac.com>
2015-09-04 14:00:26 -0700

    ImportResult status: starting, running, finishing, finished, stalled, aborted.

--- app/models/import_result.rb
+++ app/models/import_result.rb
@@ -1,6 +1,12 @@
 class ImportResult < ActiveRecord::Base
   belongs_to :import
+  enum status: [ :starting, :running, :finishing, :finished, :stalled, :aborted ]
   serialize :found_listing_keys
   serialize :removed_listing_keys
   serialize :snapshots
+  
+  def found_count_difference
+    previous_run = self.import.import_results.where(['start_time < ?', self.start_time]).order("start_time DESC").limit(1).first
+    previous_run.present? ? self.found_listing_keys.count - previous_run.found_listing_keys.count : nil
+  end
 end
--- lib/generators/reso/templates/create_import_results.rb
+++ lib/generators/reso/templates/create_import_results.rb
@@ -2,6 +2,7 @@ class CreateImportResults < ActiveRecord::Migration
   def change
     create_table :import_results do |t|
       t.references  :import, index: true, foreign_key: true
+      t.integer     :status, default: 0
       t.datetime    :source_data_modified
       t.datetime    :start_time
       t.datetime    :end_time
